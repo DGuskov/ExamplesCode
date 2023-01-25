3da74a8b6134671141a7669ac765f89c8b51ba19
Denis <denis@MacBook-Pro-Denis.local>
2023-01-25 23:46:30 +0300

    Added Task01 and Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,36 @@
+﻿// написать программу, котрая будет выдавать 
+// название дня недели по заданному номеру. 
+// например:
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.WriteLine("Введите номер дня: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine("это Понедельник");
+}
+if (number == 2)
+{
+    Console.WriteLine("Это Вторник");
+}
+if (number == 3)
+{
+    Console.WriteLine("Это Среда");
+}
+if (number == 4)
+{
+    Console.WriteLine("это Четверг");
+}
+if (number == 5)
+{
+    Console.WriteLine("Это Пятница");
+}
+if (number == 6)
+{
+    Console.WriteLine("Это Суббота");
+}
+if (number == 7)
+{
+    Console.WriteLine("Это Воскресенье");
+}
