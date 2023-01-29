f5100e39dd4aeaef7a62457267216f6af15b754f
DGuskov <denis180391@yandex.ru>
2023-01-29 10:56:20 +0300

    HomeWork - Added Task08

--- Task04H/Program.cs
+++ Task04H/Program.cs
@@ -6,24 +6,20 @@
 
 Console.WriteLine("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите третье число: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
-if (number1 > 0)
-{
-    MaxNumber = number1;    
-    Console.WriteLine("Максимальное число = " + MaxNumber);
-}
-else if (number2 > MaxNumber)
+int MaxNumber = number1;
+if (number2 > MaxNumber)
 {
     MaxNumber = number2;
-    Console.WriteLine("максимальное число = " + MaxNumber);
+    Console.WriteLine("Максимальное число = " + MaxNumber);
 }
 else if (number3 > MaxNumber)
 {
     MaxNumber = number3;
-    Console.WriteLine("Максимально число = " + MaxNumber);
+    Console.WriteLine("максимальное число = " + MaxNumber);
 }
 else
 {
---
+++ Task08H/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе
+// показывает все четные числа от 1 до N.
+// 5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+int StartNum = 1;
+Console.WriteLine("Введите натуральное число:");
+int Num = Convert.ToInt32(Console.ReadLine());
+if (Num > 0)
+{
+    int count = Num;
+    while (count % 2 == 0)
+    {
+        Console.Write(count + " ");
+        count++;
+    }
+}
+else
+{
+    Console.WriteLine("число должно быть больше одного");
+}
